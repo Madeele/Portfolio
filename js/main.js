d04ff009bd5cfa8dc07e7f961d959ab765f642e4
Maddy <55574979+Madeele@users.noreply.github.com>
2020-10-06 14:25:23 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/********** On scroll move welcoming text *********/
+// $(document).ready(function(){
+//     $(window).scroll(function() {
+//         $( ".welcoming_text" ).animate({'margin-bottom': '125%'}, 'slow');
+//     });
+// });
+
+function openNav() {
+    document.querySelector(".nav_opened").style.right = "0px";
+};
+
+function closeNav() {
+    document.querySelector(".nav_opened").style.right = "-1000px";
+    if (window.innerWidth > 400) {
+        document.querySelector(".nav_opened").style.right = "0px";
+    }
+};
